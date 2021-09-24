f9a435f018cac295b638af6368e498c38735ce35
ilsrbn <serbini271@gmail.com>
2021-09-24 17:10:35 +0300

    Поиска

--- src/views/User/Cinemas.vue
+++ src/views/User/Cinemas.vue
@@ -4,25 +4,40 @@
       <div class="container-fluid">
         <b-row>
           <b-col cols="12">
-            <b-img fluid src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Sala_de_cine.jpg"></b-img>
+            <b-img
+              fluid
+              src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Sala_de_cine.jpg"
+            ></b-img>
           </b-col>
         </b-row>
         <b-row class="mt-5">
           <b-col cols="10">
-            <h1 class="text-center">{{$t('cinemas.h1')}}</h1>
+            <h1 class="text-center">{{ $t("cinemas.h1") }}</h1>
             <b-row>
-            <b-col v-for="(cinema, index) in cinemas" :key="index" cols="6">
-              <router-link :to="{name: 'Cinema', params: {id: index}}">
-              <b-card img-top :img-src="cinema[locale].logo != '' ? cinema[locale].logo : 'https://via.placeholder.com/1600x900.png'" bg-variant="dark">
-                <b-card-title>{{cinema[locale].title}}</b-card-title>
-                <b-card-body>
-                </b-card-body>
-              </b-card>
-            </router-link>
-            </b-col>
+              <b-col v-for="(cinema, index) in cinemas" :key="index" cols="6">
+                <router-link :to="{ name: 'Cinema', params: { id: index } }">
+                  <b-card
+                    img-top
+                    :img-src="
+                      cinema[locale].logo != ''
+                        ? cinema[locale].logo
+                        : 'https://via.placeholder.com/1600x900.png'
+                    "
+                    bg-variant="dark"
+                  >
+                    <b-card-title>{{ cinema[locale].title }}</b-card-title>
+                    <b-card-body> </b-card-body>
+                  </b-card>
+                </router-link>
+              </b-col>
             </b-row>
           </b-col>
-          <b-col><b-img fluid src="https://via.placeholder.com/200x900.png?text=Контекстная+реклама"></b-img></b-col>
+          <b-col
+            ><b-img
+              fluid
+              src="https://via.placeholder.com/200x900.png?text=Контекстная+реклама"
+            ></b-img
+          ></b-col>
         </b-row>
       </div>
     </section>
@@ -30,21 +45,21 @@
 </template>
 
 <script>
-import {fetch} from '@/modules/firebase'
+import { fetch } from "@/modules/firebase";
 export default {
   data: () => ({
-    cinemas: []
+    cinemas: [],
   }),
 
   computed: {
     locale() {
-      return this.$i18n.locale
-    }
+      return this.$i18n.locale;
+    },
   },
 
   async mounted() {
-    this.cinemas = await fetch('/cinemas')
-  }
+    this.cinemas = await fetch("/cinemas");
+  },
 };
 </script>
 
